c387e0017eb5dc10f454b16a5b6e800cd1b6c15e
omar <omarqais225@gmail.com>
2026-05-01 18:08:21 +0300

    retrive everything back

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import DashboardLayout from "./pages/dashboard/DashboardLayout";
-import Home from "./pages/home/Home";
-import Users from "./pages/users/Users";
-import AddCar from "./pages/addCar/AddCar";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<DashboardLayout />}>
-          <Route index element={<Home />} />
-          <Route path="users" element={<Users />} />
-          <Route path="add-car" element={<AddCar />} />
-        </Route>
+        
       </Routes>
     </>
   );
